271c0c929cc36a6514c64594e8d808b52a3f682a
volkan <nzaro19@gmail.com>
2024-01-20 21:20:03 +0300

    testing tailwind layout and adding phpcs.xml ruleset

--- index.php
+++ index.php
@@ -1,13 +1,45 @@
 <!doctype html>
 <html>
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="/styles/styles.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/styles/styles.css" rel="stylesheet">
 </head>
+
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <!-- component -->
+    <section>
+        <div class="py-20 text-white bg-black">
+            <div class="container flex flex-col items-center mx-auto my-12 md:flex-row md:my-24">
+                <div class="flex flex-col items-start justify-center w-full p-8 lg:w-1/3">
+                    <h1 class="p-2 text-3xl text-yellow-300 md:text-5xl tracking-loose">Gideon's Army</h1>
+                    <h2 class="mb-2 text-3xl leading-relaxed md:text-5xl md:leading-snug">Daddy Owen, Dunco, Rufftone
+                    </h2>
+                    <p class="mb-4 text-sm md:text-base text-gray-50">Explore our Music.</p>
+                    <a href="#"
+                        class="px-4 py-2 text-yellow-300 bg-transparent border border-yellow-300 rounded shadow hover:bg-yellow-300 hover:text-black hover:shadow-lg hover:border-transparent">
+                        Explore Now</a>
+                </div>
+                <div class="justify-center p-8 mt-12 mb-6 ml-0 md:mb-0 md:mt-0 md:ml-12 lg:w-2/3">
+                    <div class="flex flex-wrap content-center h-48">
+                        <div>
+                            <img class="hidden inline-block mt-28 xl:block"
+                                src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png">
+                        </div>
+                        <div>
+                            <img class="inline-block p-8 mt-24 md:mt-0 md:p-0"
+                                src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png">
+                        </div>
+                        <div>
+                            <img class="hidden inline-block mt-28 lg:block"
+                                src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
+
 </html>
