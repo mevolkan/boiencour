8b5cb95f958619e6b34007a18f4ac957ac2aabfd
volkan <nzaro19@gmail.com>
2024-01-20 21:35:29 +0300

    basic markup for the pages

--- index.php
+++ index.php
@@ -41,41 +41,104 @@
         </div>
     </section>
 
-    <!-- component -->
-<div class="py-4 text-white bg-black">
-        <div class="container flex flex-col mx-auto my-12 md:flex-row md:my-36">
-      <div class="flex flex-col w-full p-8 lg:w-2/5">
-        <p class="my-4 text-3xl leading-relaxed text-yellow-500 md:text-5xl md:leading-snug">Contact our team</p>
-        <p class="my-2 font-sans text-sm md:text-lg md:my-4"><svg class="inline-block mr-2 fill-current"
-            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path
-              d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
-          </svg>Guru Nank Dev Engineering College, Ludhiana</p>
-        <p class="my-2 font-sans text-sm md:text-lg md:my-4"><svg class="inline-block mr-2 fill-current"
-            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path
-              d="M12 12.713l-11.985-9.713h23.971l-11.986 9.713zm-5.425-1.822l-6.575-5.329v12.501l6.575-7.172zm10.85 0l6.575 7.172v-12.501l-6.575 5.329zm-1.557 1.261l-3.868 3.135-3.868-3.135-8.11 8.848h23.956l-8.11-8.848z" />
-          </svg> techfestgndec@gmail.com</p>
-        <p class="my-2 font-sans text-sm md:text-lg md:my-4"><svg class="inline-block mr-2 fill-current"
-            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path
-              d="M20 22.621l-3.521-6.795c-.008.004-1.974.97-2.064 1.011-2.24 1.086-6.799-7.82-4.609-8.994l2.083-1.026-3.493-6.817-2.106 1.039c-7.202 3.755 4.233 25.982 11.6 22.615.121-.055 2.102-1.029 2.11-1.033z" />
-          </svg>+91745435433</p> <br> <br>
-        <p class="my-2 font-sans text-sm md:text-lg md:my-4">Press the chat icon at the right bottom corner to have a
-          live chat with our team.
-        </p>
-      </div>
-      <div class="flex flex-col justify-center w-full  lg:w-3/5 lg:-mt-12">
-        <div class="container">
-          <div class="relative flex flex-col w-full min-w-0 break-words">
-            <div class="flex-auto p-5 lg:p-10">
-              <img src="https://user-images.githubusercontent.com/54521023/152731049-cc744a56-1d6f-4945-9566-0fa3b7ad1d24.png" alt="contact image">
+    <!-- Artist Cards -->
+    <section>
+        <div class="py-4 text-white bg-black">
+
+            <div class="container flex flex-col items-center mx-auto my-12 md:flex-row md:my-24">
+                <div class="flex flex-col w-full p-8 lg:w-3/5">
+                    <h1 class="my-4 text-3xl leading-relaxed text-yellow-500 md:text-5xl md:leading-snug">Gideon's Army
+                    </h1>
+                    <div class="my-2 font-sans text-sm md:text-lg md:my-4">Our team has put a lot of effort to build
+                        this
+                        website
+                        and get a platform for people to register for this overall India tech fest. We really appreciate
+                        each team member's contribution and dedication towards the website.
+                        Do leave a feedback if you enjoyed being here. <br>
+                        <p>Regards,</p>
+                        <p>Gideon's Army</p>
+                    </div>
+                </div>
             </div>
-          </div>
+
+
+            <div class="container flex flex-col items-center mx-auto my-12 md:flex-row md:my-24">
+
+                <div class="w-1/2 px-1 my-4 md:w-1/3 lg:w-1/3 overflow-hiddden">
+                    <div>
+                        <img class="w-24 mx-auto transition duration-500 border-4 border-yellow-500 rounded-full hover:border-white md:mx-0 md:w-auto"
+                            src="https://user-images.githubusercontent.com/54521023/152726300-b03e134b-ff5c-40bf-a982-e01d825b4b68.png"
+                            alt="user-avatar">
+                        <p class="my-4 text-lg md:mt-8"><b>User Name</b></p>
+                        <a href="#"
+                        class="px-4 py-2 mt-4 text-yellow-300 bg-transparent border border-yellow-300 rounded shadow hover:bg-yellow-300 hover:text-black hover:shadow-lg hover:border-transparent">
+                        Explore Music</a>
+                    </div>
+                </div>
+
+                <div class="w-1/2 px-1 my-4 md:w-1/3 lg:w-1/3">
+                    <div>
+                        <img class="w-24 mx-auto transition duration-500 border-4 border-yellow-500 rounded-full hover:border-white md:mx-0 md:w-auto"
+                            src="https://user-images.githubusercontent.com/54521023/152726300-b03e134b-ff5c-40bf-a982-e01d825b4b68.png"
+                            alt="user-avatar">
+                        <p class="mt-4 text-lg md:mt-8"><b>User Name</b></p>
+                        <p class="text-yellow-500">Designation</p>
+                    </div>
+                </div>
+
+                <div class="w-1/2 px-1 my-4 md:w-1/3 lg:w-1/3">
+                    <div>
+                        <img class="w-24 mx-auto transition duration-500 border-4 border-yellow-500 rounded-full hover:border-white md:mx-0 md:w-auto"
+                            src="https://user-images.githubusercontent.com/54521023/152726300-b03e134b-ff5c-40bf-a982-e01d825b4b68.png"
+                            alt="user-avatar">
+                        <p class="mt-4 text-lg md:mt-8"><b>User Name</b></p>
+                        <p class="text-yellow-500">Designation</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
-      </div>
+
+    </section>
+
+    <!-- Footer -->
+    <div class="py-4 text-white bg-black">
+        <div class="container flex flex-col mx-auto my-12 md:flex-row md:my-36">
+            <div class="flex flex-col w-full p-8 lg:w-2/5">
+                <p class="my-4 text-3xl leading-relaxed text-yellow-500 md:text-5xl md:leading-snug">Contact our team
+                </p>
+                <p class="my-2 font-sans text-sm md:text-lg md:my-4"><svg class="inline-block mr-2 fill-current"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                            d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
+                    </svg>Guru Nank Dev Engineering College, Ludhiana</p>
+                <p class="my-2 font-sans text-sm md:text-lg md:my-4"><svg class="inline-block mr-2 fill-current"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                            d="M12 12.713l-11.985-9.713h23.971l-11.986 9.713zm-5.425-1.822l-6.575-5.329v12.501l6.575-7.172zm10.85 0l6.575 7.172v-12.501l-6.575 5.329zm-1.557 1.261l-3.868 3.135-3.868-3.135-8.11 8.848h23.956l-8.11-8.848z" />
+                    </svg> techfestgndec@gmail.com</p>
+                <p class="my-2 font-sans text-sm md:text-lg md:my-4"><svg class="inline-block mr-2 fill-current"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                            d="M20 22.621l-3.521-6.795c-.008.004-1.974.97-2.064 1.011-2.24 1.086-6.799-7.82-4.609-8.994l2.083-1.026-3.493-6.817-2.106 1.039c-7.202 3.755 4.233 25.982 11.6 22.615.121-.055 2.102-1.029 2.11-1.033z" />
+                    </svg>+91745435433</p> <br> <br>
+                <p class="my-2 font-sans text-sm md:text-lg md:my-4">Press the chat icon at the right bottom corner to
+                    have a
+                    live chat with our team.
+                </p>
+            </div>
+            <div class="flex flex-col justify-center w-full lg:w-3/5 lg:-mt-12">
+                <div class="container">
+                    <div class="relative flex flex-col w-full min-w-0 break-words">
+                        <div class="flex-auto p-5 lg:p-10">
+                            <img src="https://user-images.githubusercontent.com/54521023/152731049-cc744a56-1d6f-4945-9566-0fa3b7ad1d24.png"
+                                alt="contact image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 </body>
 
 </html>
