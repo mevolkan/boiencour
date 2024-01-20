52bdc7fdfdbbabc37308062df754b55f0e268789
volkan <nzaro19@gmail.com>
2024-01-20 23:13:48 +0300

    test db connection

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,11 @@
 <?php
+
+
 $host = "localhost";
 $user = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $dbname =  getenv('DB_NAME');
+
+echo $user;
 
 $con = mysqli_connect($host, $user, $password, $dbname);
