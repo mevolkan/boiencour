39e18533360f4855a588831dc6a810c90bf4c31c
volkan <16579273+mevolkan@users.noreply.github.com>
2024-01-22 23:41:39 +0300

    cleanup

--- includes/header.php
+++ includes/header.php
@@ -16,22 +16,44 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
-        <link href="./assets/css/now-ui-kit.css?v=1.3.0" rel="stylesheet" />
     <link rel="stylesheet" href="./assets/css/styles.css" type="text/css" />
     <script type="text/javascript">
         function edt_id(id) {
-            window.location.href = 'edit_artist.php?edit_id=' + id;
+            window.location.href = 'edit.php?edit_id=' + id;
         }
         function view_id(id) {
-            window.location.href = 'view_artist.php?view_id=' + id;
+            window.location.href = 'view.php?view_id=' + id;
         }
         function delete_id(id) {
             if (confirm('Sure to Delete ?')) {
-                window.location.href = 'indexartist.php?delete_id=' + id;
+                window.location.href = 'index.php?delete_id=' + id;
             }
         }
         function changestatus_id(id, status) {
-            window.location.href = 'indexartist.php?changestatus_id=' + id + '&status=' + status;
+            window.location.href = 'index.php?changestatus_id=' + id + '&status=' + status;
         }
     </script>
 </head>
+
+<nav class="container navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="#">
+    <img src="/docs/4.6/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Gideon's Army
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Artists</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Albums</a>
+      </li>
+    </ul>
+  </div>
+</nav>
