5cfbff95fece71f4b3cba30b5b20cb893017f1dd
volkan <16579273+mevolkan@users.noreply.github.com>
2024-01-22 23:04:03 +0300

    adding stylesheets

--- includes/header.php
+++ includes/header.php
@@ -4,14 +4,20 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Core PHP Crud functions By PHP Code Builder</title>
+    <title>Gideons Army</title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css" type="text/css" />
+        <link href="./assets/css/now-ui-kit.css?v=1.3.0" rel="stylesheet" />
+    <link rel="stylesheet" href="./assets/css/styles.css" type="text/css" />
     <script type="text/javascript">
         function edt_id(id) {
             window.location.href = 'edit_artist.php?edit_id=' + id;
@@ -29,5 +35,3 @@
         }
     </script>
 </head>
-
-<body>
